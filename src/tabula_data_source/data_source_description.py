21adea203d28858a031c18ddeae9deb82e39d839
runekri3 <kristofer.kirss@hotmail.com>
2023-07-07 23:15:19 +0300

    feat: Add tabula data source description

---
+++ src/tabula_data_source/data_source_description.py
@@ -0,0 +1,19 @@
+from typing import Literal
+
+from description_models.base_data_source_description import BaseDataSourceDescription
+from pydantic import Field
+
+TABULA_DATA_SOURCE_DESCRIPTION_TYPE = "tabula-table"
+TABULA_DATA_SOURCE_DESCRIPTION_TYPE_LITERAL = Literal["tabula-table"]
+
+
+class TabulaDataSourceDescription(BaseDataSourceDescription):
+    """Description of getting data using Tabula."""
+
+    type: TABULA_DATA_SOURCE_DESCRIPTION_TYPE_LITERAL
+
+    page: int
+    page_table_number: int = Field(
+        default=1,
+        description="The sequential number of the table on the page.",
+    )
