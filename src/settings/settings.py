0918cf7851d5d695f8e086dc0844871bde7821f9
runekri3 <kristofer.kirss@hotmail.com>
2023-07-09 03:10:15 +0300

    feat: Use project path as root for default paths, instead of current working directory

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -6,19 +6,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .log_level import LogLevel
 
+PROJECT_PATH = Path(__file__).parent.parent.parent
+
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env',
+    model_config = SettingsConfigDict(env_file=PROJECT_PATH / '.env',
                                       env_file_encoding='utf-8')
 
     log_level: LogLevel = LogLevel.INFO
 
     books_path: Path = Field(
         description="Folder path where books are loaded from",
-        default_factory=lambda: Path(".") / "data" / "books")
+        default_factory=lambda: PROJECT_PATH / "data" / "books")
     book_descriptions_path: Path = Field(
         description="Folder path where book description files are loaded from",
-        default_factory=lambda: Path(".") / "book_descriptions")
+        default_factory=lambda: PROJECT_PATH / "book_descriptions")
 
 
 try:
