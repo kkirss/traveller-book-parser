37d04b00e996cff7b7c5b5bd47b4e861b5ca0f7a
runekri3 <kristofer.kirss@hotmail.com>
2023-07-12 03:55:51 +0300

    fix: Ignore additional env vars for settings

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=PROJECT_PATH / '.env',
         env_file_encoding='utf-8',
+        extra="ignore",
     )
 
     log_level: LogLevel = LogLevel.INFO
