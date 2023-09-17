c7118a586c79dc4151e0cd2768796e050885dae7
kkirss <kristofer.kirss@hotmail.com>
2023-09-17 20:33:24 +0300

    feat: Change CLI commands to output database as JSON

--- traveller_book_parser/cli_app/entities.py
+++
@@ -1,6 +0,0 @@
-from traveller_book_parser.traveller_models.entity import Entity
-
-
-def print_entities(entities: list[Entity]):
-    for entity in entities:
-        print(entity)
--- traveller_book_parser/cli_app/parse.py
+++ traveller_book_parser/cli_app/parse.py
@@ -1,15 +1,13 @@
 from traveller_book_parser.books.parse_books import parse_all_books, parse_book_entities
 from traveller_book_parser.traveller_models.traveller_database import TravellerDatabase
 
-from .entities import print_entities
-
 
 def parse_book_cli(book_code_name: str):
     """Parse a book and print processed entities."""
     database = TravellerDatabase()
     parse_book_entities(database, book_code_name)
 
-    print_entities(database.all_entities)
+    print(database.model_dump_json(indent=2))
 
 
 def parse_all_books_cli():
@@ -17,4 +15,4 @@ def parse_all_books_cli():
     database = TravellerDatabase()
     parse_all_books(database)
 
-    print_entities(database.all_entities)
+    print(database.model_dump_json(indent=2))
