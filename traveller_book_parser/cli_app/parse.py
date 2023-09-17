7a28ed435f944fea92659344b793890a0953ad24
kkirss <kristofer.kirss@hotmail.com>
2023-09-17 15:07:02 +0300

    fix: Fix CLI parse-book expecting book path, not code name

--- traveller_book_parser/cli_app/parse.py
+++ traveller_book_parser/cli_app/parse.py
@@ -1,13 +1,11 @@
-from pathlib import Path
-
 from traveller_book_parser.books.parse_books import parse_all_books, parse_book
 
 from .entities import print_entities
 
 
-def parse_book_cli(book_path: Path):
+def parse_book_cli(book_code_name: str):
     """Parse a book and print processed entities."""
-    entities = parse_book(book_path)
+    entities = parse_book(book_code_name)
     print_entities(entities)
 
 
