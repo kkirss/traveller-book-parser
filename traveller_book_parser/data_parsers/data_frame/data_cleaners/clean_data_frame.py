51215241a7fd6d675dd5b40f13ef36976b501b30
kkirss <kristofer.kirss@hotmail.com>
2023-09-17 19:47:45 +0300

    feat: Log cleaning type with cleaned data frame

--- traveller_book_parser/data_parsers/data_frame/data_cleaners/clean_data_frame.py
+++ traveller_book_parser/data_parsers/data_frame/data_cleaners/clean_data_frame.py
@@ -14,9 +14,9 @@ from .unnamed_columns import remove_unnamed_columns
 logger = logging.getLogger(__name__)
 
 
-def log_cleaned_data_frame(data_frame: DataFrame) -> None:
+def log_cleaned_data_frame(data_frame: DataFrame, clean_type: str) -> None:
     if SETTINGS.log_intermediate_data:
-        logger.debug("Cleaned data frame:\n%s", data_frame)
+        logger.debug("Cleaned data frame (%s):\n%s", clean_type, data_frame)
 
 
 def clean_data_frame(input_data_frame: DataFrame) -> DataFrame:
@@ -27,22 +27,22 @@ def clean_data_frame(input_data_frame: DataFrame) -> DataFrame:
     while True:
         if (new_data_frame := remove_unnamed_columns(data_frame)) is not None:
             data_frame = new_data_frame
-            log_cleaned_data_frame(data_frame)
+            log_cleaned_data_frame(data_frame, remove_unnamed_columns.__name__)
             continue
 
         if (new_data_frame := collapse_almost_empty_rows(data_frame)) is not None:
             data_frame = new_data_frame
-            log_cleaned_data_frame(data_frame)
+            log_cleaned_data_frame(data_frame, collapse_almost_empty_rows.__name__)
             continue
 
         if (new_data_frame := spread_returns_to_multiple_rows(data_frame)) is not None:
             data_frame = new_data_frame
-            log_cleaned_data_frame(data_frame)
+            log_cleaned_data_frame(data_frame, spread_returns_to_multiple_rows.__name__)
             continue
 
         if (new_data_frame := replace_few_returns_with_spaces(data_frame)) is not None:
             data_frame = new_data_frame
-            log_cleaned_data_frame(data_frame)
+            log_cleaned_data_frame(data_frame, replace_few_returns_with_spaces.__name__)
             continue
 
         return data_frame
