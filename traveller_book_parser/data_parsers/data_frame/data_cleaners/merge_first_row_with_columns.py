e2e04644150187e17e8ceb051b1f61325342646b
kkirss <kristofer.kirss@hotmail.com>
2024-05-28 05:17:03 +0300

    test: Add doctest to merge_first_row_with_columns

--- traveller_book_parser/data_parsers/data_frame/data_cleaners/merge_first_row_with_columns.py
+++ traveller_book_parser/data_parsers/data_frame/data_cleaners/merge_first_row_with_columns.py
@@ -4,7 +4,15 @@ from pandas import DataFrame
 
 
 def merge_first_row_with_columns(input_data_frame: DataFrame) -> DataFrame | None:
-    """Merge the first row of a data frame with the columns."""
+    """Merge the first row of a data frame with the columns.
+
+    >>> merge_first_row_with_columns(DataFrame([
+    ...     ["", "", "extra"],
+    ...     ["a", "b", "c"],
+    ... ], columns=["header1", "header2", "header"]))
+      header1 header2 header extra
+    1       a       b            c
+    """
     data_frame = input_data_frame
     columns = data_frame.columns.to_list()
 
