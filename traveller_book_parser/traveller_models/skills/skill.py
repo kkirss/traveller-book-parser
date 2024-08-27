e36d6a485cf69b9a4342c86211664a71618f9785
kkirss <kristofer.kirss@hotmail.com>
2024-08-28 00:04:59 +0300

    feat: Merge BaseSkill into Skill

--- traveller_book_parser/traveller_models/skills/skill.py
+++ traveller_book_parser/traveller_models/skills/skill.py
@@ -1,12 +1,20 @@
-from typing import Any
+from typing import Any, Literal, Optional
 
-from pydantic import RootModel
+from pydantic import RootModel, Field
 
-from .base_skill import BaseSkill
+from traveller_book_parser.traveller_models.trav_object_base import TravObjectBase
+from traveller_book_parser.traveller_models.trav_object_types import TravObjectType
 
-Skill = BaseSkill
+
+class Skill(TravObjectBase):
+    """Skill with optional level."""
+
+    type: Literal[TravObjectType.SKILL] = TravObjectType.SKILL
+
+    # None signifies not knowing a skill
+    level: Optional[int] = Field(repr=True)
 
 
 def create_skill(**kwargs: Any) -> Skill:  # noqa: ANN401
     """Create Skill model instance."""
-    return RootModel[Skill](**kwargs).root
+    return Skill(**kwargs)
--- traveller_book_parser/traveller_models/validators/skill_from_name.py
+++ traveller_book_parser/traveller_models/validators/skill_from_name.py
@@ -7,7 +7,7 @@ def skill_from_name(value: Any) -> Optional[Skill]:  # noqa: ANN401
     """Convert space-separated name and level to a Skill.
 
     >>> skill_from_name("Melee (blade) 1")
-    BaseSkill(type=<TravObjectType.SKILL: 'skill'>, name='Melee (blade)', image_path=None, level=1)
+    Skill(type=<TravObjectType.SKILL: 'skill'>, name='Melee (blade)', image_path=None, level=1)
     """
     if isinstance(value, str):
         name, level = value.rsplit(" ", maxsplit=1)
