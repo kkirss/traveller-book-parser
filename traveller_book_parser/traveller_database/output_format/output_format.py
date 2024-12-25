9ed62d58740f0b8c8d03a5f0401a67b7811f2bda
kkirss <kristofer.kirss@hotmail.com>
2024-12-25 22:26:32 +0200

    feat: Add cheat_sheet_pdf output format stubs
    Formatting itself is not implemented yet.

--- traveller_book_parser/traveller_database/output_format/output_format.py
+++ traveller_book_parser/traveller_database/output_format/output_format.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 from traveller_book_parser.traveller_models.trav_database import TravDatabase
 
+from .cheat_sheet_pdf import format_database_cheat_sheet_pdf
 from .json import format_database_json
 from .none import format_database_none
 
@@ -10,6 +11,7 @@ from .none import format_database_none
 class OutputFormat(str, Enum):
     """Output format (e.g. json or none)."""
 
+    cheat_sheet_pdf = "cheat_sheet_pdf"
     json = "json"
     none = "none"
 
@@ -17,6 +19,7 @@ class OutputFormat(str, Enum):
 DatabaseOutputFormatter = Callable[[TravDatabase], str]
 
 DATABASE_OUTPUT_FORMATTERS: dict[OutputFormat, DatabaseOutputFormatter] = {
+    OutputFormat.cheat_sheet_pdf: format_database_cheat_sheet_pdf,
     OutputFormat.json: format_database_json,
     OutputFormat.none: format_database_none,
 }
