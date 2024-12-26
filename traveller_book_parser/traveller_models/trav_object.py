19880eb6b37c313a5c3ed440bbaa4ed9029117d2
kkirss <kristofer.kirss@hotmail.com>
2024-12-26 03:29:04 +0200

    feat: Simplify TravObject docstring

--- traveller_book_parser/traveller_models/trav_object.py
+++ traveller_book_parser/traveller_models/trav_object.py
@@ -7,20 +7,7 @@ from .characters.character import Character
 from .items.item import Item
 from .skills.skill import Skill
 
-TRAV_OBJ_DESCRIPTION = """A traveller-related object.
-
-Traveller objects are defined as different sub-models, using `type` field to differentiate them:
-* `characteristic` - A characteristic of a character (e.g. strength).
-* `item` - An item (e.g. sword, phone, book).
-* `skill` - A skill (e.g. athletics, pilot, science).
-* `character` - A character.
-
-Each type of traveller object has its own fields. Different types may have further sub-types with further fields.
-These are intended to be usable with any edition of Traveller. (But completely untested so Your Mileage May Vary.)
-
-Run `traveller-book-parser schema TravObject` for the full JSON schema.
-See [`traveller_book_parser/traveller_models/trav_object.py`](traveller_book_parser/traveller_models/trav_object.py) for the actual models.
-"""
+TRAV_OBJ_DESCRIPTION = """A traveller-related object."""
 
 TravObjectField = Field(
     discriminator="type",
@@ -32,13 +19,11 @@ TravObject = Annotated[
     TravObjectField,
 ]
 
-
-class TravObjectRoot(RootModel[TravObject]):  # noqa: D101
-    ...
-
-
 TravObject.__doc__ = TRAV_OBJ_DESCRIPTION
-TravObjectRoot.__doc__ = TRAV_OBJ_DESCRIPTION
+
+
+class TravObjectRoot(RootModel[TravObject]):
+    """A traveller-related object."""
 
 
 def create_trav_object(**kwargs: Any) -> TravObject:  # noqa: ANN401
