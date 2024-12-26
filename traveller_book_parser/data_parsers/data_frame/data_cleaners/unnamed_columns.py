a4d9b31b503b4368f44e5bab9145bd35859b6e48
kkirss <kristofer.kirss@hotmail.com>
2024-12-26 02:20:02 +0200

    feat: Update non-fixed dependencies

--- traveller_book_parser/data_parsers/data_frame/data_cleaners/unnamed_columns.py
+++ traveller_book_parser/data_parsers/data_frame/data_cleaners/unnamed_columns.py
@@ -32,7 +32,7 @@ def remove_unnamed_columns(input_data_frame: DataFrame) -> Optional[DataFrame]:
     for index, column in enumerate(columns):
         if is_unnamed_column(column):
             unnamed_columns_count += 1
-            columns[index] = ""
+            columns[index] = ""  # pyright: ignore [reportIndexIssue]
 
     if unnamed_columns_count > (len(columns) / 2):
         data_frame.columns = columns
